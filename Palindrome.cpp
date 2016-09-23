69a7c5b326f76044da7e1987cf8045e46bcba36b
Elaine Yang <299747@beaverton.k12.or.us>
2016-09-23 14:18:25 -0700

    finished

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,3 +1,6 @@
+//Elaine Yang
+//Palindrome
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,27 +12,36 @@ using namespace std;
 
 int main(){
 
-	char input [80];
+	// Initializes different variable to be used later
+	char input [81];
 	int charcount = 0;
 	bool ispalindrome = false;
 	
+	// Asks for/gets size of input
 	cout << "Enter input\n";
-	cin >> input;
-	
-	int initialsize = sizeof(input) / sizeof(input[0]);
+	cin.getline(input, sizeof(input));
+	int initialsize = strlen(input);
+
+	// Counts how many characters there are
 	for(int i = 0; i < initialsize; i++){
 		if(isalpha(input[i]) || isdigit(input[i])){
 			charcount++;
 		}
 	}
+	
+	// Creates new array to hold only characters
 	char validchars [charcount];
 	charcount = 0;
+
+	// Puts chars into new array
 	for(int i = 0; i < initialsize; i++){
 		if(isalpha(input[i]) || isdigit(input[i])){
 			validchars[charcount] = input[i];
 			charcount++;
 		}
 	}
+
+	// Check if corresponding chars are the same
 	for(int i = 0; i < (charcount/2); i++){
 		if(validchars[i] == validchars[charcount-i-1]){
 			ispalindrome = true;
@@ -39,6 +51,8 @@ int main(){
 			break;
 		}
 	}
+
+	// Print result
 	if(ispalindrome == true){
 		cout << "Palindrome";
 	}
